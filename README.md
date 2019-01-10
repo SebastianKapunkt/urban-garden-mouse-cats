4a8fb271d53810a9823bd65dad79dd146e9df56b
Sebastian Kapunkt <kindt.Sebastian@outlook.com>
2019-01-10 16:25:13 +0100

    inital commit - basic setup dotnet & infer

---
+++ README.md
@@ -0,0 +1,3 @@
+# Urban guard and the story of mouse and cats
+
+This is a repository for first touches with infer.net with a quite fictional simulation example.
---
+++ urban_garden_mouse_cats/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace urban_garden_mouse_cats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
